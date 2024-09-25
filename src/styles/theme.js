d90878d8ce973109faaa04f93cf0966f8eefa9fb
sergiopassinho <sergiolpassinho@gmail.com>
2024-09-24 23:25:36 -0300

    ajustes navbar e banner

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -35,40 +35,28 @@ export const gradients = {
 
 export const buttonSizes = {
     extraLarge: {
-        // width: '226px',
         height: '60px',
-        top: '133px',
-        left: '64px',
         borderRadius: '51px',
         padding: '12px 24px',
         gap: '10px',
         fontSize: '20px'
     },
     large: {
-        // width: '167px',
         height: '52px',
-        top: '137px',
-        left: '316px',
         borderRadius: '30px',
         padding: '12px 24px',
         gap: '10px',
         fontSize: '16px'
     },
     medium: {
-        // width: '157px',
         height: '42px',
-        top: '141px',
-        left: '534px',
         padding: '10px 18px',
         gap: '10px',
         borderRadius: '38px',
         fontSize: '16px'
     },
     small: {
-        // width: '127px',
         height: '32px',
-        top: '147px',
-        left: '752px',
         borderRadius: '45px',
         padding: '8px 16px',
         gap: '10px',
